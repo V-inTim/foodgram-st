888277d75a731e91d27e9afb790a81bfb1a4ba21
Виничук Тимофей <102575697+V-inTim@users.noreply.github.com>
2025-05-05 23:37:58 +0300

    Change  recipes.

--- backend/recipes/filters.py
+++ backend/recipes/filters.py
@@ -5,23 +5,27 @@ from .models import Recipe
 
 
 class RecipeFilter(FilterSet):
-    is_favorited = filters.BooleanFilter(
-        method="filter_is_favorited"
-    )
-    is_in_shopping_cart = filters.BooleanFilter(
+    is_favorited = filters.CharFilter(method="filter_is_favorited")
+    is_in_shopping_cart = filters.CharFilter(
         method="filter_is_in_shopping_cart"
     )
 
     def filter_is_favorited(self, queryset, name, value):
-        if value and self.request.user.is_authenticated:
-            return queryset.filter(recipe_favorite__user=self.request.user)
+        if value in {'1', '0'}:
+            value = bool(int(value))
+            if value and self.request.user.is_authenticated:
+                return queryset.filter(
+                    recipe_favorite__user=self.request.user,
+                )
         return queryset
 
     def filter_is_in_shopping_cart(self, queryset, name, value):
-        if value and self.request.user.is_authenticated:
-            return queryset.filter(
-                recipe_shopping_list__user=self.request.user
-            )
+        if value in {'1', '0'}:
+            value = bool(int(value))
+            if value and self.request.user.is_authenticated:
+                return queryset.filter(
+                    recipe_shopping_list__user=self.request.user,
+                )
         return queryset
 
     class Meta:
