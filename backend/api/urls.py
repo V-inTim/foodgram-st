40df57cb4c9b17e7bfef3827055c4ec7b3e3405f
Виничук Тимофей <102575697+V-inTim@users.noreply.github.com>
2025-05-08 15:15:28 +0300

    Move urls.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -2,14 +2,15 @@ from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
 from ingredients.views import IngredientViewSet
+from recipes.views import RecipeViewSet
 
 
 router = DefaultRouter()
 router.register("ingredients", IngredientViewSet)
+router.register("recipes", RecipeViewSet)
 
 urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')),
     path('users/', include('users.urls')),
-    path('recipes/', include('recipes.urls')),
     path('', include(router.urls)),
 ]
