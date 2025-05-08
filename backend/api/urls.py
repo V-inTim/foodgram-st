a94f163ca099ecb3326268630dd88759bd2d3f82
Виничук Тимофей <102575697+V-inTim@users.noreply.github.com>
2025-05-08 12:37:34 +0300

    Move ingredients urls to api urls.

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -1,8 +1,15 @@
 from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from ingredients.views import IngredientViewSet
+
+
+router = DefaultRouter()
+router.register("ingredients", IngredientViewSet)
 
 urlpatterns = [
     path('auth/', include('djoser.urls.authtoken')),
     path('users/', include('users.urls')),
-    path('ingredients/', include('ingredients.urls')),
     path('recipes/', include('recipes.urls')),
+    path('', include(router.urls)),
 ]
