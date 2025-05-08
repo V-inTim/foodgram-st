83d3c23c5cff5daccc76f31fc9a6881652980dce
Виничук Тимофей <102575697+V-inTim@users.noreply.github.com>
2025-05-08 12:38:28 +0300

    Change Ingredient model.

--- backend/ingredients/serializers.py
+++ backend/ingredients/serializers.py
@@ -3,6 +3,8 @@ from .models import Ingredient
 
 
 class IngredientSerializer(serializers.ModelSerializer):
+    """Привязан к модели Ingredient."""
+
     class Meta:
         model = Ingredient
         fields = ['id', 'name', 'measurement_unit']
